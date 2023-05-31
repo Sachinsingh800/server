7a1cedbed7e42a53d246cde332ce2a1b9d306852
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-31 13:19:49 +0530

    studentForm

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://sachinsinghgnc:VPHB1JEwU3U0gMkD@computer-site.vpqxzwp.mongodb.net/computer-site")
 
 const userRoute=require("./routes/userRoute")
+
+const studentRoute=require("./routes/studentRoute")
+app.use('/api',studentRoute)
 app.use('/api',userRoute)
 app.listen(8000,()=>{
     console.log("server is runing... on port 8000")
---
+++ userController/studentController.js
@@ -0,0 +1,63 @@
+const Student = require("../Modals/studentModal");
+const form = async (req, res) => {
+
+    try{
+       const name=req.body.name
+       const email=req.body.email
+       const image= req.file.filename
+       const mobile=req.body.mobile
+       const gender=req.body.gender
+       const dob=req.body.dob
+       const fatherName=req.body.fatherName
+       const motherName=req.body.motherName
+       const parentContact=req.body.parentContact
+       const address=req.body.address
+       const adharNo=req.body.adharNo
+       const course=req.body.course
+       const studentDetail=await  Student.findOne({email})
+       if(studentDetail){
+          res.status(201).send({message:"abe kitni bar ek hi form ko submitg karega berojgar kahika"}) 
+       }else{
+        
+           const student=new Student({
+               name,
+               email,
+               mobile,
+               image: req.file.filename,
+               gender,
+               dob,
+               fatherName,
+               motherName,
+               parentContact,
+               address,
+               adharNo,
+               course,
+         
+               
+           })
+           const studentDetails=await student.save()
+           res.status(200).send({message:"bhai tumhara form submit ho gya hai ab thoda ruk",studentDetails})
+       }
+       
+    }catch(error){
+       console.log(error.message)
+    }
+ 
+}
+const getFromData=async(req,res)=>{
+         try{
+          const userinfo= await Student.find({})
+    
+          if(userinfo){
+            res.status(200).send({message:"your user detail fetch successfuly",userinfo})
+          }else{
+            res.status(201).send({message:"user information not found"})
+          }
+         }catch(error){
+            res.status(400).send({message:"Something wrong please check your internet"})
+         }
+}
+module.exports = {
+  form,
+  getFromData
+};
